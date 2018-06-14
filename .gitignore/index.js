dac42f95e8750f26d8d1bf46a1dfaf73a179d740
Dwenarra <40275676+Dwenarra@users.noreply.github.com>
2018-06-14 22:21:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 var prefix = "!";
 
-client.login("NDQ5NjI4NTUyNTUyMjUxMzkz.Dencyg.rYeVxSXO5LC32pMQYCNYedUGhvA");
+client.login(process.env.TOKEN);
 
 client.on("ready", () => {
     console.log("Je suis prêt !")
